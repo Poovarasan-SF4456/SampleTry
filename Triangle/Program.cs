fb65bd8a661e7b4abc69d42e10eae2440976e9de
Poovarasan-SF4456 <159987746+Poovarasan-SF4456@users.noreply.github.com>
2024-04-18 09:36:55 +0530

    Triangle-SF4456
    This is About the Triangle in C#

---
+++ Triangle/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Reflection.Metadata;
+
+//Console.WriteLine("Hello, World!");
+
+int number=int.Parse(Console.ReadLine());
+for(int i=0;i<number;i++){
+    for(int j=0;j<=number;j++){
+        Console.Write("*"+" ");
+    }
+    Console.WriteLine();
+}
+
